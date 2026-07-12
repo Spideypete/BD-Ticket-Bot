6e26b95d157df7b0ab208b421c68934c6783bad3
Spidey <spidey@example.com>
2026-07-12 23:49:18 +0300

    Fix figlet banner crash on Node.js 20 by replacing with plain text

--- src/lib/banner.js
+++ src/lib/banner.js
@@ -1,18 +1,12 @@
 /* eslint-disable no-console */
 const { colours } = require('leeks.js');
-const figlet = require('figlet');
 const link = require('terminal-link');
 
 module.exports = version => {
-	figlet
-		.textSync('Discord', { font: 'Banner3' })
-		.split('\n')
-		.forEach(line => console.log(colours.cyan(line)));
-	console.log('');
-	figlet
-		.textSync('Tickets', { font: 'Banner3' })
-		.split('\n')
-		.forEach(line => console.log(colours.cyan(line)));
+	console.log(colours.cyan('==='));
+	console.log(colours.cyan('  Discord'));
+	console.log(colours.cyan('  Tickets'));
+	console.log(colours.cyan('==='));
 	console.log('');
 	console.log(colours.cyanBright(`${link('Discord Tickets', 'https://discordtickets.app')} bot v${version} by eartharoid`));
 	console.log(colours.cyanBright('Sponsor this project at https://discordtickets.app/sponsor'));
